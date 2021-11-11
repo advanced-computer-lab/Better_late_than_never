92a0b4c28cb5b3133b87fe72932db35a6d69ef20
hamo0od <47895342+hamo0od@users.noreply.github.com>
2021-11-11 18:08:31 +0200

    new update
    hamed ashraf

--- Routes/api/flight.js
+++ Routes/api/flight.js
@@ -1,84 +1,55 @@
-const express = require('express');
-const flight = require('../../models/flight');
+const express = require("express");
+const flight = require("../../models/flight");
 const router = express.Router();
-const user=require('../../models/user')
-const Flight = require('../../models/flight');
+const user = require("../../models/user");
+const Flight = require("../../models/flight");
 
+router.get("/test", (req, res) => res.send("flight route testing!"));
 
-router.get('/test', (req, res) => res.send('flight route testing!'));
-
-
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Flight.find()
-    .then(flight => res.json(flight))
-    .catch(err => res.status(404).json({ noFlightsFound: 'No Flights found' }));
-});
-
-
-
-router.get('/:id', (req, res) => {
-  Flight.findById(req.params.id)
-    .then(flight => res.json(flight))
-    .catch(err => res.status(404).json({ noFlightsFound: 'No Flights found' }));
-});
-
-
-
-router.post('/', (req, res) => {
-  Flight.create(req.body)
-    .then(flight => res.json({ msg: 'flight added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this flight' }));
-});
-
-// 
-
-router.post('/:id', (req, res) => {
-  Flight.findByIdAndUpdate(req.params.id, req.body)
-    .then(flight => res.json({ msg: 'Updated successfully' }))
-    .catch(err =>
-      res.status(400).json({ error: 'Unable to update the Database' })
+    .then((flight) => res.json(flight))
+    .catch((err) =>
+      res.status(404).json({ noFlightsFound: "No Flights found" })
     );
 });
+router.get("/find-and-update", (req, res) => {
+  db.grades.findOneAndUpdate({ Number: req.body.number });
+});
 
-router.post('/create-flight',(req,res)=>{
+//
+
+router.post("/a7a", (req, res) => {
+  console.log("here");
   const u = new flight({
-
-   
-    flightNumber:req.body.flightNumber,
-    departureTime:req.body.departureTime,
-    arrivalTime:req.body.arrivalTime,
-    date:req.body.date,
-    noOfEconomySeats:req.body.noOfBuseinessSeats,
-    noOfBuseinessSeats:req.body.noOfBuseinessSeats,
-    airport:req.body.airport
-    });
-console.log(u)
-    u .save()
-    .then(() => res.json('mbrook !!111!!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-
-
-
-})
-router.post('/create-user',(req,res)=>{
-
+    flightNumber: req.body.flightNumber,
+    departureTime: req.body.departureTime,
+    arrivalTime: req.body.arrivalTime,
+    date: req.body.date,
+    noOfEconomySeats: req.body.noOfBuseinessSeats,
+    noOfBuseinessSeats: req.body.noOfBuseinessSeats,
+    airport: req.body.airport,
+  });
+  console.log(u);
+  u.save()
+    .then(() => res.json("mbrook !!111!!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+router.post("/create-user", (req, res) => {
   const x = new user({
-   name:req.body.username,
-   password:req.body.pass
-   
-    });
-console.log(x)
-    x.save()
-    .then(() => res.json('mbrook !!111!!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    name: req.body.username,
+    password: req.body.pass,
+  });
 
-
-
-})
-router.delete('/:id', (req, res) => {
+  console.log(x);
+  x.save()
+    .then(() => res.json("mbrook !!111!!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+router.delete("/:id", (req, res) => {
   Flight.findByIdAndRemove(req.params.id, req.body)
-    .then(flight => res.json({ mgs: 'Flight entry deleted successfully' }))
-    .catch(err => res.status(404).json({ error: 'No such Flight' }));
+    .then((flight) => res.json({ mgs: "Flight entry deleted successfully" }))
+    .catch((err) => res.status(404).json({ error: "No such Flight" }));
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,44 +1,35 @@
-const express = require('express');
-var cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-// routes
-const flight = require('./Routes/api/flight');
+require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-// Connect Database
-const db = "mongodb+srv://dbZiad:ah@cluster0.c86mb.mongodb.net/airline?retryWrites=true&w=majority"
-
+app.use(cors());
+app.use(express.json());
+const db =
+  "mongodb+srv://hamed:123@cluster0.z8i29.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 try {
-     mongoose.connect(
-      db,
-      {
-        useNewUrlParser: true
-      }
-    ).then( console.log('MongoDB is Connected...'+" "+db))
-   
-  } catch (err) {
-      console.log("hello");
-    console.error(err.message);
-    process.exit(1);
-  }
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("MongoDB is Connected..." + " " + db));
+} catch (err) {
+  console.log("hello");
+  console.error(err.message);
+  process.exit(1);
+}
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+const flightrouter = require("./Routes/api/flight");
+app.use("/flight", flightrouter);
 
-// cors
-app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-app.use('/flight', flight);
-app.get('/', (req, res) => res.send('Hello world!'));
-app.post('/create-flight',(req,res)=> res.send('a7a'))
-
-
-// use Routes
-app.use('/api/flight', flight);
-//app.use('/components/createUser');
-
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log("Server is running on port:" + port);
+});
